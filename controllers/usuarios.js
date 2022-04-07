f12e3818ddcf44816f8747b010fc04ae0641f0e5
mario <vigossalamanca@hotmail.com>
2022-04-06 21:49:59 -0500

    actualizacion de usuario de google para qque no puedan cambiar el coreo

--- controllers/usuarios.js
+++ controllers/usuarios.js
@@ -136,7 +136,14 @@ const actualizarUsuario =  async(req, res = response) => {
             }
         }
 
-        campos.email = email;
+        if(!usuarioDB.google){
+            campos.email = email;
+        }else if(usuarioDB.email !== email){
+            return res.status(400).json({
+                ok: false,
+                msg: 'Usuario del google no puede cambiar su correo'
+            });
+        }
 
         // campos.email = email;
         // // lo hacemos otra vez para que no meta esos campos
